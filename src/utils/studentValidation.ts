294f8c1d4e8fcdab8eb6d78463698caefac79060
Santiago Mancera <dsmr19961@gmail.com>
2023-04-19 22:38:30 -0500

    Creacion de la funcion isValid para validar inputs

---
+++ src/utils/studentValidation.ts
@@ -0,0 +1,23 @@
+import StylesForm from "../styles/StylesForm.js";
+
+export default function isValid(input:HTMLInputElement):Boolean {
+    if (input.value == "") {
+        StylesForm.alertInput(input);
+        return false;
+    }
+    
+    if(input.type == "tel" && input.value.length < 6){
+        return false;
+    }
+
+    if (input.type == "email" && !input.value.includes("@") || !input.value.includes(".com")) {
+        return false;
+    }
+
+    if (input.type == "url" && !input.value.startsWith("https://")) {
+        return false;
+    }
+
+    return true;
+
+}
