4db026459145a933ca4d6ca75e0b7ba49785d416
Santiago Mancera <dsmr19961@gmail.com>
2023-04-19 22:34:56 -0500

    Creacion del modelo Student

---
+++ src/models/studentModel.ts
@@ -0,0 +1,10 @@
+type Student ={
+    tipoIdentificacion: number;
+    numeroIdentificacion: number;
+    nombres: string;
+    apellidos: string;
+    celular: number;
+    correo: string;
+    linkedin: string;
+    github: string; 
+}
