01c394d3bcd44736ae120b35ad4cf71c498c34fc
Santiago Mancera <dsmr19961@gmail.com>
2023-04-21 18:16:01 -0500

    Modificacion del nombre del archivo studentModel -> studentPostModel y creacion de studentGetModel

---
+++ src/models/studentGetModel.ts
@@ -0,0 +1,12 @@
+interface StudentGet{
+    estudiante_apellidos:string;
+    estudiante_celular?:string;
+    estudiante_correo:string;
+    estudiante_estado:string
+    estudiante_fechaCreacion?:string;
+    estudiante_github?:string
+    estudiante_id:number
+    estudiante_linkedin?:string;
+    estudiante_nombres:string;
+}
+export default StudentGet;
---
+++ src/models/studentPostModel.ts
@@ -0,0 +1,11 @@
+interface StudentPost{
+    tipoIdentificacion: number;
+    numeroIdentificacion: number;
+    nombres: string;
+    apellidos: string;
+    celular: number;
+    correo: string;
+    linkedin: string;
+    github: string; 
+}
+export default StudentPost;
