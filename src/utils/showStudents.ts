613b2fd5ce68e0ada398f350fc607570a4a05f3f
Santiago Mancera <dsmr19961@gmail.com>
2023-04-22 23:48:49 -0500

    Agregando los eventListener a los botones Detalle y CambiarEstatus

--- src/utils/showStudents.ts
+++ src/utils/showStudents.ts
@@ -1,4 +1,7 @@
 import StudentGet from "../models/studentGetModel.js";
+import token from "../connection/accessToken.js";
+import showStudentDetail from "./showStudentDetail.js";
+import updateStatus from "../events/eventChagedStatusStudent.js";
 
 const tableBodyStudents = document.getElementById("tableBodyStudents") as HTMLTableSectionElement;
 
@@ -25,41 +28,58 @@ export default function showStudents(studentsArray:StudentGet[]){
         
         // Campo estado
         const newTdState = document.createElement("td");
-        newTdState.textContent = student.estudiante_estado;
+        newTdState.textContent = student.estudiante_estado.toLowerCase();
 
         newTrElement.append(newThId,
                             newTdName,
                             newTdLasname,
                             newTdEmail,
                             newTdState,
-                            createButtonsActions());
+                            createButtonsActions(student.estudiante_id,student.estudiante_estado));
 
         tableBodyStudents.appendChild(newTrElement);
     });
 
 }
 
-function createButtonsActions():HTMLTableCellElement{
+function createButtonsActions(idStudent:number,statusStudent:string):HTMLTableCellElement{
     const newTdButtonsActions = document.createElement("td");
+    newTdButtonsActions.classList.add("text-center")
 
+    // Boton para editar el estudiante
     const buttonEdit = document.createElement("button");
     buttonEdit.type = "button";
     buttonEdit.classList.add("btn","btn-secondary","mx-1");
     buttonEdit.textContent = "Editar"
 
+    // Boton para cambiar el estado del estudiante
     const buttonChangeStatus = document.createElement("button");
     buttonChangeStatus.type = "button";
-    buttonChangeStatus.classList.add("btn","btn-danger","mx-1");
-    buttonChangeStatus.textContent = "Desactivar";
+    let classButtonStatus = "";
+    let textButtonStatus = "";
+    if (statusStudent.toLowerCase() == "activo") {
+        classButtonStatus = "btn-danger";
+        textButtonStatus = "Desactivar";
+    }else{
+        classButtonStatus = "btn-success";
+        textButtonStatus = "Activar"
+    }
+    buttonChangeStatus.classList.add("btn",classButtonStatus,"mx-1");
+    buttonChangeStatus.textContent = textButtonStatus;
+    // Cambio de estado
+    buttonChangeStatus.addEventListener("click",()=> updateStatus(token,idStudent,statusStudent))
 
+    // Boton detalle
     const buttonInfoStudent = document.createElement("button");
     buttonInfoStudent.type = "button";
     buttonInfoStudent.classList.add("btn","btn-info","mx-1");
     buttonInfoStudent.textContent = "Detalle";
+    buttonInfoStudent.setAttribute("data-bs-toggle","modal");
+    buttonInfoStudent.setAttribute("data-bs-target","#staticBackdrop");
+    // Detalle del estudiante
+    buttonInfoStudent.addEventListener("click",()=> showStudentDetail(token,idStudent));
 
-    newTdButtonsActions.append(buttonEdit,
-                                buttonChangeStatus,
-                                buttonInfoStudent);   
+    newTdButtonsActions.append(buttonEdit,buttonChangeStatus,buttonInfoStudent);   
 
     return newTdButtonsActions;
 }
