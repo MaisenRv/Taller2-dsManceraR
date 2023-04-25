433bf006e057f1559cf4206d13d413ec9c572639
Santiago Mancera <dsmr19961@gmail.com>
2023-04-24 20:58:37 -0500

    Creando el metodo putDataStudentToEdit -> trae los datos del estudiante y lo pone en los inputs del modal

---
+++ src/utils/putDataStudentToEdit.ts
@@ -0,0 +1,26 @@
+import getStudent from "../controllers/getStudent.js";
+import StudentGet from "../models/studentGetModel.js";
+
+function putDataStudentToEdit(token:string, idStudent:number) {
+    const idStudentShow = document.getElementById("idStudentShow") as HTMLLabelElement;
+    const numberIdentificationShow = document.getElementById("numberIdentificationShow") as HTMLLabelElement;
+    const nombresEdit = document.getElementById("nombresEdit") as HTMLInputElement;
+    const apellidosEdit = document.getElementById("apellidosEdit") as HTMLInputElement;
+    const celularEdit = document.getElementById("celularEdit") as HTMLInputElement;
+    const emailEdit = document.getElementById("emailEdit") as HTMLInputElement;
+    const linkedinEdit = document.getElementById("linkedinEdit") as HTMLInputElement;
+    const gitHubEdit = document.getElementById("gitHubEdit") as HTMLInputElement;
+
+    getStudent(token,idStudent).then(student=>{
+        idStudentShow.textContent = "ID: " + student.estudiante_id.toString();
+        numberIdentificationShow.textContent = "Numero de identificación: " + student.estudiante_numeroIdentificacion!.toString();
+        nombresEdit.value = student.estudiante_nombres;
+        apellidosEdit.value = student.estudiante_apellidos;
+        celularEdit.value = student.estudiante_celular!;
+        emailEdit.value = student.estudiante_correo;
+        linkedinEdit.value = student.estudiante_linkedin!;
+        gitHubEdit.value = student.estudiante_github!;
+    })
+}
+
+export default putDataStudentToEdit;
