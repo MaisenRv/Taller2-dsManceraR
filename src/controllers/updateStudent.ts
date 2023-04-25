add4f37a0b745df41b51a5a8f202a865505953dc
Santiago Mancera <dsmr19961@gmail.com>
2023-04-24 20:56:08 -0500

    Creando el metodo que actualiza los estudiantes

---
+++ src/controllers/updateStudent.ts
@@ -0,0 +1,19 @@
+import StudentPut from "../models/studentPutModel.js";
+
+async function updateStudent(token:string, idStudent:number ,updatedStudent:StudentPut) {
+    const header = new Headers();
+
+    header.append("Content-Type","application/json");
+    header.append("Authorization", "Bearer " + token);
+    const request = {
+        method: "PUT",
+        headers: header,
+        body: JSON.stringify(updatedStudent)
+    }
+
+    const response = await fetch("https://apiestudiantes.maosystems.dev/estudiantes/"+idStudent, request);
+    const data = await response.json();
+    console.log(data);
+    
+}
+export default updateStudent;
