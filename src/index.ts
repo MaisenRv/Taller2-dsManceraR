5e556a0873e4fcc728cd450338d05d617bb21f91
Santiago Mancera <dsmr19961@gmail.com>
2023-04-24 23:13:31 -0500

    Se quito el evento loadInitialData y se puso en el index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hola desde la rama dev_santiago");
+import getAllStudents from "./controllers/getAllStudents.js";
+import showStudents from "./utils/showStudents.js";
+
+// Mi token
+import token from "./connection/accessToken.js";
+getAllStudents(token).then(studentsData=> showStudents(studentsData));
+
+const btnCloseModalEditStudent = document.getElementById("btnCloseModalEditStudent");
+btnCloseModalEditStudent!.addEventListener("click",()=>{
+    localStorage.clear();
+})
