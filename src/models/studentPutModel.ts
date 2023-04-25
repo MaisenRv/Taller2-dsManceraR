f0c9c8e2ea0a8d586a89681430bf91b9a4d988bc
Santiago Mancera <dsmr19961@gmail.com>
2023-04-24 20:57:16 -0500

    Creando el modelo put de estudiantes para su actualizacion

---
+++ src/models/studentPutModel.ts
@@ -0,0 +1,9 @@
+interface StudentPut{
+    apellidos?:string;
+    celular?:string;
+    correo?:string;
+    github?:string
+    linkedin?:string;
+    nombres?:string;
+}
+export default StudentPut;
